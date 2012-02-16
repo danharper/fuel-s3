7e0cd1c91ebc8686f9ef8f70bb37283d222479bf
Dan Harper <intouch@danharper.me>
2012-02-16 14:00:19 +0000

    S3Exception extend FuelException

--- classes/s3/exception.php
+++ classes/s3/exception.php
@@ -3,10 +3,10 @@
 namespace S3;
 
 class S3Exception extends \FuelException {
-	function __construct($message, $file, $line, $code = 0)
-	{
-		parent::__construct($message, $code);
-		$this->file = $file;
-		$this->line = $line;
-	}
+	// function __construct($message, $file, $line, $code = 0)
+	// {
+	// 	parent::__construct($message, $code);
+	// 	$this->file = $file;
+	// 	$this->line = $line;
+	// }
 }
